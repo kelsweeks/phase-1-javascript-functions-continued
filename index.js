233a7cbe5da085bed74536e3c852aec9ba6c7e48
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-06-09 16:26:47 -0500

    commit tree fd5572f87c68ca66b903a47d9c4d620b847b98c5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+const mondayWork = function(activity="go to the office") {
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(flair="*") {
+    (adj="Special") => `You are ${flair + adj + flair}!`
+}
+
+function wrapAdjective(string = "*") {
+    return `You are ${string}${string}!`
+    wrapAdjective()(parameter = 'special')
+        return `You are ${string}${parameter}${string}!`
+  }
+
+//  function wrapAdjective(flair="*") {
+//    (adj="Special") => `You are ${flair + adj + flair}!`
+//}
+//function wrapAdjective() {
+//    return function inner(default='*') {
+//    return `You are ${default}!`
+//    }
+//}
